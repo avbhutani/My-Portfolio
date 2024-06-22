c67aa0c96413ef92c29a1331b40315fc90e85001
avbhutani <avbhutani3@gmail.com>
2024-06-22 20:16:03 +0530

    Integrated the navbar UI, links still to be fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 // import Navbar from './components/navbar/Navbar'
 import Start from './components/start/Start';
 import FloatingNavbar from './components/floatingNavbar/FloatingNavbar'
-import Navbar from './components/navbar/Navbar'
 import About from './components/about/About';
 import Contact from './components/contactMe/Contact';
 import ProudThings from './components/proudThings/ProudThings';
+// import EducationalTimeline from './components/educationTimeline/EducationalTimeLine';
 function App() {
   return (
     <div className="App">
@@ -14,11 +14,13 @@ function App() {
       <About />
       <div className='proud-things'>
       <h1>Most Proud Of</h1>
-      <ProudThings headerContent = 'ok this is the first one' bodyContent = 'This is the first box' />
-      <ProudThings headerContent = 'ok this is the first one' bodyContent = 'This is the first box' />
-      <ProudThings headerContent = 'ok this is the first one' bodyContent = 'This is the first box' />
+      <ProudThings headerContent = 'SELECTED AS FSE SUMMER INTERN AT FIDELITY INVESTMENTS, BENGALURU' bodyContent = 'This is the first box' />
+      <ProudThings headerContent = 'RECEIVED SRM JEE MERIT SCHOLARSHIP WORTH Rs. 7,50,000' bodyContent = 'This is the first box' />
+      <ProudThings headerContent = 'PARTICIPATED IN CODE FOR GOOD 2024 BY JP MORGAN CHASE' bodyContent = 'This is the first box' />
       </div>
       <Contact />
+      <FloatingNavbar />
+      {/* <EducationalTimeline /> */}
     </div>
   );
 }
