f7e797abbb5f76752677bdbfa46145d4efbae0b3
avbhutani <avbhutani3@gmail.com>
2024-10-18 20:45:02 +0530

    Fix/ Proud Things Section

--- frontend/src/components/proudThings/ProudThings.js
+++ frontend/src/components/proudThings/ProudThings.js
@@ -2,11 +2,10 @@ import React from 'react'
 import { useCollapse } from 'react-collapsed'
 import './ProudThings.css'
 function ProudThings(props) {
-  const { getToggleProps} = useCollapse()
 
   return ( 
     <>
-    <div className='collapsible'{...getToggleProps()}>
+    <div className='collapsible'>
         <div className='collapsible-header' >
         <div className='collapsible-header-text'>
         <span className='collapsible-header-1'>+</span> <span className='collapsible-header-2'>{props.headerContent}</span>
